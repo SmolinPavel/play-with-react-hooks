341213489c7bc682d36c8fb1320611c2c2479a00
Pavel Smolin <isingwithaz@gmail.com>
2018-11-07 15:22:32 +0300

    Add Carousel component in the start version of it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './MediaWithHooks';
+import './Carousel/whatevs/index.css';
+import App from './Carousel/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

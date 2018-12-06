1eec4e8a4b61b2e3b8c5e856edcb37bd42665dde
Pavel Smolin <isingwithaz@gmail.com>
2018-12-06 10:46:20 +0300

    Extend Buttons component children

--- src/ReactClone.js
+++ src/ReactClone.js
@@ -1,36 +1,46 @@
 import React, { Children, cloneElement, Component } from 'react';
 
 class ReactClone extends Component {
-    render() {
-        return (
-            <Buttons>
-                <button value="A">A</button>
-                <button value="B">B</button>
-                <button value="C">C</button>
-            </Buttons>
-        );
-    }
+  render() {
+    return (
+      <Buttons>
+        <button value="A">A</button>
+        <button value="B">B</button>
+        <button value="C">C</button>
+        <Button value="D" />
+      </Buttons>
+    );
+  }
 }
 
+const Button = ({ onClick, value }) => (
+  <button value={value} onClick={onClick}>
+    Button component
+  </button>
+);
+
 class Buttons extends Component {
-    state = { selected: 'None' };
+  state = { selected: 'None' };
 
-    selectItem = selected => {
-        this.setState({ selected })
-    };
+  selectItem = selected => {
+    this.setState({ selected });
+  };
 
-    render() {
-        const { children } = this.props;
-        const fn = child => cloneElement(child, { onClick: () => this.selectItem(child.props.value) });
-        const items = Children.map(children, fn);
+  render() {
+    const { children } = this.props;
+    const fn = child =>
+      cloneElement(child, {
+        onClick: () => this.selectItem(child.props.value)
+      });
+    const items = Children.map(children, fn);
 
-        return(
-            <div>
-                <h2>You've selected: {this.state.selected}</h2>
-                {items}
-            </div>
-        )
-    }
+    return (
+      <div>
+        <h2>You've selected: {this.state.selected}</h2>
+        {items}
+      </div>
+    );
+  }
 }
 
 export default ReactClone;

94ead071922cc6d5e23fec166a0cc8eea6e3f781
Pavel Smolin <isingwithaz@gmail.com>
2018-12-09 15:41:37 +0300

    Add more mock data

--- src/ContextPresentation/Shared/mock.js
+++ src/ContextPresentation/Shared/mock.js
@@ -8,5 +8,11 @@ export const messages = [
   { id: 7, text: 'Dummy text 6' },
   { id: 8, text: 'Dummy text 7' },
   { id: 9, text: 'Dummy text 8' },
-  { id: 10, text: 'Dummy text 9' }
+  { id: 10, text: 'Dummy text 9' },
+  { id: 11, text: 'Dummy text 10' },
+  { id: 12, text: 'Dummy text 11' },
+  { id: 13, text: 'Dummy text 12' },
+  { id: 14, text: 'Dummy text 13' },
+  { id: 15, text: 'Dummy text 14' },
+  { id: 16, text: 'Dummy text 15' }
 ];

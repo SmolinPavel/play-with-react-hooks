2c08f9a16e4cbbcfac00ff3c4874128538472694
Pavel Smolin <isingwithaz@gmail.com>
2018-12-12 17:00:16 +0300

    Setup for the css-grid demo

---
+++ src/CssGridDemo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.scss';
+
+const CssGrid = () => (
+  <div className="container">
+    <div className="header">Header</div>
+    <div className="trophies">Trophies</div>
+    <div className="info">Info</div>
+    <div className="photo">Photo</div>
+    <div className="bio">Bio</div>
+  </div>
+);
+
+export default CssGrid;
